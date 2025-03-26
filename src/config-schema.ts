d23e8ae3d6e329cb5f569aee799ce017a3f4dd18
rishabhsharma3579@gmail.com <rishabhsharma3579@gmail.com>
2025-03-26 20:26:47 +0530

    Validation Done

---
+++ src/config-schema.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod'
+
+const upstreamSchema = z.object({
+    id: z.string(),
+    url: z.string().url()
+});
+
+const headerSchema = z.object({
+    key: z.string(),
+    value: z.string(),
+});
+
+const rulesSchema = z.object({
+    path: z.string(),
+    upstreams : z.array(z.string()),
+});
+
+const serverSchema = z.object({
+    listen: z.number(),
+    workers: z.number().optional(),
+    upstreams: z.array(upstreamSchema),
+    headers: z.array(headerSchema).optional(),
+    rules: z.array(rulesSchema),
+});
+
+export const rootConfigSchema = z.object({
+    server: serverSchema
+})
