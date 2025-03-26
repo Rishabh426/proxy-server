a79dd89e5ce9c6e36e7073db348e348cc5772441
rishabhsharma3579@gmail.com <rishabhsharma3579@gmail.com>
2025-03-26 21:53:02 +0530

    Master process spinned worker processes

--- dist/index.js
+++ dist/index.js
@@ -8,16 +8,41 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
 const config_1 = require("./config");
+const node_cluster_1 = __importDefault(require("node:cluster"));
+const node_os_1 = __importDefault(require("node:os"));
+const node_http_1 = __importDefault(require("node:http"));
+function createServer(config) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { workerCount } = config;
+        if (node_cluster_1.default.isPrimary) {
+            console.log("Master process is on 🚀");
+            for (let i = 0; i < workerCount; i++) {
+                node_cluster_1.default.fork();
+                console.log(`Master process : Worker node spinned up ${i}`);
+            }
+            const server = node_http_1.default.createServer((req, res) => { });
+        }
+        else {
+            console.log(`Worker node 🚀`);
+        }
+        const workers = new Array(workerCount);
+    });
+}
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
+        var _a;
         commander_1.program.option('--config <path>');
         commander_1.program.parse();
         const options = commander_1.program.opts();
         if (options && 'config' in options) {
             const validatedConfig = yield (0, config_1.validateConfig)(yield (0, config_1.parseYAMLConfig)(options.config));
+            yield createServer({ port: validatedConfig.server.listen, workerCount: (_a = validatedConfig.server.workers) !== null && _a !== void 0 ? _a : node_os_1.default.cpus().length });
             console.log(validatedConfig);
         }
     });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
 import { program } from 'commander'
 import { parseYAMLConfig, validateConfig } from './config'
+import cluster from 'node:cluster'
+import { workerData } from 'node:worker_threads';
+import os from 'node:os'
+import http from 'node:http'
+
+interface CreateServerConfig {
+    port: number,
+    workerCount: number,
+}
+
+async function createServer(config: CreateServerConfig) {
+    const { workerCount } = config;
+    if(cluster.isPrimary) {
+        console.log("Master process is on 🚀");
+
+        for(let i = 0; i < workerCount; i++) {
+            cluster.fork(); 
+            console.log(`Master process : Worker node spinned up ${i}`);
+        }
+
+        const server = http.createServer((req, res) => {})
+    } 
+    else {
+        console.log(`Worker node 🚀`);
+    }
+    const workers = new Array(workerCount);
+}   
 
 async function main() {
     program.option('--config <path>');
@@ -10,6 +37,7 @@ async function main() {
         const validatedConfig = await validateConfig(
             await parseYAMLConfig(options.config)
         );
+        await createServer({port: validatedConfig.server.listen, workerCount: validatedConfig.server.workers ?? os.cpus().length});
         console.log(validatedConfig);
     }
 }
